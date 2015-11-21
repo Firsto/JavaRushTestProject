14ad7208a73a9d149229dfbdd642305e0a0bae9d
razor <zaoroman@gmail.com>
2015-11-21 11:15:54 +0700

    init

---
+++ src/main/java/ru/firsto/JavaRushTestProjectApplication.java
@@ -0,0 +1,12 @@
+package ru.firsto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRushTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRushTestProjectApplication.class, args);
+    }
+}
